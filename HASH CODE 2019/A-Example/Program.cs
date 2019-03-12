2225c9b6d4a7e442db020190069f569e0a838d42
Muhammad Rafay <46870378+MuhammadRafay151@users.noreply.github.com>
2019-03-12 15:37:31 +0500

    Update Program.cs

--- HASH CODE 2019/A-Example/Program.cs
+++ HASH CODE 2019/A-Example/Program.cs
@@ -14,29 +14,12 @@ namespace A_Example
             
             string[][] images;
             images = s1.Load_Images(@"C:\Users\HP\Downloads\b_lovely_landscapes.txt");
-            //for (int i = 0; i < images.Length; i++)
-            //{
-            //    Console.Write(i+">>");
-            //    foreach (string j in images[i])
-            //    {
-            //        Console.Write(j + " ");
-            //    }
-            //    Console.WriteLine(); 
-            //}
-            //Console.WriteLine(">>>>>>>>>>>>>>>");
+            
           
             Image[] slides = s1.VerticalMerge(images);
             Console.WriteLine(slides.Length);
              slides = s1.CreateShow(slides);
-            //Console.WriteLine(22);
-            //for(int i=0;i<slides.Length;i++)
-            //{
-            //    foreach(int j in slides[i].GetImage())
-            //    {
-            //        Console.Write(j-1+" ");
-            //    }
-            //    Console.WriteLine();
-            //}
+           
              s1.GenerateOutput(slides);
 
         }
